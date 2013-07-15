56d914cb38cb8c49bdf3569567c6c8fa421c3c7a
John Markham <seanomarachain@gmail.com>
2013-07-15 22:51:53 +0100

    Changes

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-  response.send(buffer.toString('utf-8'));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
